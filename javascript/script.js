73e1fad4807e44de4e18d4497e0467d1ba2e0040
randywande <randyahmat76@gmail.com>
2022-06-12 14:07:37 +0700

    add function x and localStorage

--- javascript/script.js
+++ javascript/script.js
@@ -54,11 +54,25 @@ const performCalculation = () => {
 	if (calculator.operator === "+") {
 		result =
 			parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-	} else {
+	}
+	if (calculator.operator === "-") {
 		result =
 			parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
 	}
+	if (calculator.operator === "x") {
+		result =
+			parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
+	}
+
+	const history = {
+		firstNumber: calculator.firstNumber,
+		secondNumber: calculator.displayNumber,
+		operator: calculator.operator,
+		result: result,
+	};
+	putHistory(history);
 	calculator.displayNumber = result;
+	renderHistory();
 };
 
 const buttons = document.querySelectorAll(".button");
@@ -94,3 +108,53 @@ for (let button of buttons) {
 		updateDisplay();
 	});
 }
+// local storage
+const CACHE_KEY = "calculation_history";
+
+const checkForStorage = () => {
+	if (typeof Storage !== "undefined") {
+	}
+};
+
+const putHistory = data => {
+	if (checkForStorage()) {
+		let historyData = null;
+		if (localStorage.getItem(CACHE_KEY) === null) {
+			historyData = [];
+		} else {
+			historyData = JSON.parse(localStorage.getItem(CACHE_KEY));
+		}
+
+		historyData.unshift(data);
+
+		if (historyData.length > 5) {
+			historyData.pop();
+		}
+	}
+};
+
+const showHistory = () => {
+	if (checkForStorage()) {
+		return JSON.parse(localStorage.getItem(CACHE_KEY) || []);
+	} else {
+		return [];
+	}
+};
+
+const renderHistory = () => {
+	const historyData = showHistory();
+	let historyList = document.querySelector("#historyList");
+
+	// selalu hapus konten HTML pada elemen historyList agar tidak menampilkan data ganda
+	historyList.innerHTML = "";
+
+	for (let history of historyData) {
+		let row = document.createElement("tr");
+		row.innerHTML = "<td>" + history.firstNumber + "</td>";
+		row.innerHTML += "<td>" + history.operator + "</td>";
+		row.innerHTML += "<td>" + history.secondNumber + "</td>";
+		row.innerHTML += "<td>" + history.result + "</td>";
+
+		historyList.appendChild(row);
+	}
+};
